ba30325f718ab329769b41f065dd90fb6ee7e14d
Gerardo Ramirez <Brownsiey@Gerardos-MBP.hsd1.ca.comcast.net>
2018-08-10 20:19:01 -0700

    final push!

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Liri Project
 
 This project uses two API's; the Spotify API, and the OMDB API. In order to do a music search, simply type in process.argv "spotify-this" followed by a space and your keywords. The same logic applies for a movie search, except you will replace "spotify-this" with omdb-this". 
+
+However, this application is still not fully functional. I will need to come into office hours to figure out the correct process, but I think I am close.
--- liri.js
+++ liri.js
@@ -18,28 +18,7 @@
     var spotify = new Spotify(keys.spotify);
 
     // ===========
-     
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
-      console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 5));
-      console.log(JSON.stringify(data.tracks.items[0].name, null, 5));
-      console.log(JSON.stringify(data.tracks.items[0].preview_url, null, 5));
-      console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 5));
-      // console.log(JSON.stringify(data, null, 5))
-    });
 
-
-
-
-
-    // =============
-
-
-
-
-    
   //   omdb.search('Title', function(err, movies) {
   //     if (err) {
   //         return console.error(err);
@@ -53,39 +32,46 @@
   //         console.log('%s (%d)', movie.title, movie.year);
   //     });
   // });
-   
-    omdb.get({ title: movieName }, true, function(err, movie) {
-      if (err) {
-          return console.error(err);
-      }
-      if (!movie) {
-          return console.log('Movie not found!');
-      }
-        console.log(movie.title);
-        console.log(movie.year);
-        console.log(movie.imdb.rating);
-        console.log(movie.plot);
-        console.log(movie.country);
-        console.log(movie.actors);
-    });
 
-    // =============
 
-// Then run a request to the OMDB API with the movie specified
-    request("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy", function(error, response, body) {
+    // request("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy", function(error, response, body) {
     
-    if (!error && response.statusCode === 200) {
+    // if (!error && response.statusCode === 200) {
 
 // Parse the body of the site and recover just the imdbRating
 // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating + "/10");
-    console.log(JSON.parse(body));
-    };
+    // console.log("The movie's rating is: " + JSON.parse(body).imdbRating + "/10");
+    // console.log(JSON.parse(body));
+    // };
 
     // ================
 
     if (parse.argv[2] === 'spotify-this') {
-        function() //spotify function??
+        spotify.search({ type: 'track', query: songName }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+            console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 5));
+            console.log(JSON.stringify(data.tracks.items[0].name, null, 5));
+            console.log(JSON.stringify(data.tracks.items[0].preview_url, null, 5));
+            console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 5));
+            // console.log(JSON.stringify(data, null, 5))
+          });
     } else {
-        omdb() // function???
+        (process.argv[2] === 'omdb-this') {
+            omdb.get({ title: movieName }, true, function(err, movie) {
+                if (err) {
+                    return console.error(err);
+                }
+                if (!movie) {
+                    return console.log('Movie not found!');
+                }
+                  console.log(movie.title);
+                  console.log(movie.year);
+                  console.log(movie.imdb.rating);
+                  console.log(movie.plot);
+                  console.log(movie.country);
+                  console.log(movie.actors);
+              });
+        }
     };
